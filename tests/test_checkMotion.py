2b3562af449b48832407ec07cbd9f21c7d2bdfa7
Genzo4 <genzo@bk.ru>
2022-06-02 17:30:31 +1000

    change slashes

--- tests/test_checkMotion.py
+++ tests/test_checkMotion.py
@@ -4,68 +4,68 @@ from motion_detector_g4 import MotionDetector
 
 def test_01_1():
     md = MotionDetector()
-    md.applyFirstFrame('tests\\images\\01_frame_1.png')
-    assert md.checkMotion('tests\\images\\01_frame_2.png') == False
+    md.applyFirstFrame('tests/images/01_frame_1.png')
+    assert md.checkMotion('tests/images/01_frame_2.png') == False
 
 
 def test_01_2():
     md = MotionDetector()
-    md.applyFirstFrame('tests\\images\\01_frame_2.png')
-    assert md.checkMotion('tests\\images\\01_frame_3.png') == False
+    md.applyFirstFrame('tests/images/01_frame_2.png')
+    assert md.checkMotion('tests/images/01_frame_3.png') == False
 
 
 def test_01_3():
     md = MotionDetector()
-    md.applyFirstFrame('tests\\images\\01_frame_3.png')
-    assert md.checkMotion('tests\\images\\01_frame_4.png') == False
+    md.applyFirstFrame('tests/images/01_frame_3.png')
+    assert md.checkMotion('tests/images/01_frame_4.png') == False
 
 
 def test_01_4():
     md = MotionDetector(minArea=2000)
-    md.applyFirstFrame('tests\\images\\01_frame_1.png')
-    assert md.checkMotion('tests\\images\\01_frame_2.png') == True
+    md.applyFirstFrame('tests/images/01_frame_1.png')
+    assert md.checkMotion('tests/images/01_frame_2.png') == True
 
 
 def test_01_5():
     md = MotionDetector(minArea=2000)
-    md.applyFirstFrame('tests\\images\\01_frame_2.png')
-    assert md.checkMotion('tests\\images\\01_frame_3.png') == True
+    md.applyFirstFrame('tests/images/01_frame_2.png')
+    assert md.checkMotion('tests/images/01_frame_3.png') == True
 
 
 def test_01_6():
     md = MotionDetector(minArea=2000)
-    md.applyFirstFrame('tests\\images\\01_frame_1.png')
-    assert md.checkMotion('tests\\images\\01_frame_2.png') == True
-    assert md.checkMotion('tests\\images\\01_frame_3.png') == True
+    md.applyFirstFrame('tests/images/01_frame_1.png')
+    assert md.checkMotion('tests/images/01_frame_2.png') == True
+    assert md.checkMotion('tests/images/01_frame_3.png') == True
 
 
 def test_01_7():
     md = MotionDetector()
-    md.applyFirstFrame('tests\\images\\01_frame_1.png')
-    assert md.checkMotion('tests\\images\\01_frame_2.png') == False
-    assert md.checkMotion('tests\\images\\01_frame_3.png') == False
+    md.applyFirstFrame('tests/images/01_frame_1.png')
+    assert md.checkMotion('tests/images/01_frame_2.png') == False
+    assert md.checkMotion('tests/images/01_frame_3.png') == False
 
 
 def test_02_1():
     md = MotionDetector()
-    md.applyFirstFrame('tests\\images\\02_frame_1.png')
-    assert md.checkMotion('tests\\images\\02_frame_2.png') == False
+    md.applyFirstFrame('tests/images/02_frame_1.png')
+    assert md.checkMotion('tests/images/02_frame_2.png') == False
 
 
 def test_03_1():
     md = MotionDetector()
-    md.applyFirstFrame('tests\\images\\03_frame_1.bmp')
-    assert md.checkMotion('tests\\images\\03_frame_2.bmp') == False
+    md.applyFirstFrame('tests/images/03_frame_1.bmp')
+    assert md.checkMotion('tests/images/03_frame_2.bmp') == False
 
 
 def test_03_2():
     md = MotionDetector()
-    md.applyFirstFrame('tests\\images\\03_frame_1.bmp')
-    assert md.checkMotion('tests\\images\\03_frame_2.bmp') == False
-    assert md.checkMotion('tests\\images\\03_frame_3.bmp') == False
+    md.applyFirstFrame('tests/images/03_frame_1.bmp')
+    assert md.checkMotion('tests/images/03_frame_2.bmp') == False
+    assert md.checkMotion('tests/images/03_frame_3.bmp') == False
 
 
 def test_03_3():
     md = MotionDetector()
-    md.applyFirstFrame('tests\\images\\03_frame_3.bmp')
-    assert md.checkMotion('tests\\images\\03_frame_4.bmp') == True
+    md.applyFirstFrame('tests/images/03_frame_3.bmp')
+    assert md.checkMotion('tests/images/03_frame_4.bmp') == True
--- tests/test_debugMod.py
+++ tests/test_debugMod.py
@@ -16,10 +16,10 @@ def _removeTempFiles() -> None:
     :return: None
     """
 
-    removeFiles = glob.iglob('tests\\images\\*.clear.*')
-    removeFiles = chain(removeFiles, glob.iglob('tests\\images\\*.mask.*'))
-    removeFiles = chain(removeFiles, glob.iglob('tests\\images\\*.blobs.*'))
-    removeFiles = chain(removeFiles, glob.iglob('tests\\images\\*.blobs2.*'))
+    removeFiles = glob.iglob('tests/images/*.clear.*')
+    removeFiles = chain(removeFiles, glob.iglob('tests/images/*.mask.*'))
+    removeFiles = chain(removeFiles, glob.iglob('tests/images/*.blobs.*'))
+    removeFiles = chain(removeFiles, glob.iglob('tests/images/*.blobs2.*'))
 
     for _file in removeFiles:
         os.remove(_file)
@@ -27,43 +27,43 @@ def _removeTempFiles() -> None:
 
 def test_1():
     md = MotionDetector()
-    md.applyFirstFrame('tests\\images\\01_frame_1.png')
-    md.checkMotion('tests\\images\\01_frame_2.png')
-    assert os.path.exists('tests\\images\\01_frame_2.clear.png') == False
-    assert os.path.exists('tests\\images\\01_frame_2.mask.png') == False
-    assert os.path.exists('tests\\images\\01_frame_2.blobs.png') == False
-    assert os.path.exists('tests\\images\\01_frame_2.blobs2.png') == False
+    md.applyFirstFrame('tests/images/01_frame_1.png')
+    md.checkMotion('tests/images/01_frame_2.png')
+    assert os.path.exists('tests/images/01_frame_2.clear.png') == False
+    assert os.path.exists('tests/images/01_frame_2.mask.png') == False
+    assert os.path.exists('tests/images/01_frame_2.blobs.png') == False
+    assert os.path.exists('tests/images/01_frame_2.blobs2.png') == False
     _removeTempFiles()
 
 
 def test_2():
     md = MotionDetector(debug=True)
-    md.applyFirstFrame('tests\\images\\01_frame_1.png')
-    md.checkMotion('tests\\images\\01_frame_2.png')
-    assert os.path.exists('tests\\images\\01_frame_2.clear.png') == True
-    assert os.path.exists('tests\\images\\01_frame_2.mask.png') == True
-    assert os.path.exists('tests\\images\\01_frame_2.blobs.png') == False
-    assert os.path.exists('tests\\images\\01_frame_2.blobs2.png') == False
+    md.applyFirstFrame('tests/images/01_frame_1.png')
+    md.checkMotion('tests/images/01_frame_2.png')
+    assert os.path.exists('tests/images/01_frame_2.clear.png') == True
+    assert os.path.exists('tests/images/01_frame_2.mask.png') == True
+    assert os.path.exists('tests/images/01_frame_2.blobs.png') == False
+    assert os.path.exists('tests/images/01_frame_2.blobs2.png') == False
     _removeTempFiles()
 
 
 def test_3():
     md = MotionDetector(minArea=2000, debug=True)
-    md.applyFirstFrame('tests\\images\\01_frame_1.png')
-    md.checkMotion('tests\\images\\01_frame_2.png')
-    assert os.path.exists('tests\\images\\01_frame_2.clear.png') == True
-    assert os.path.exists('tests\\images\\01_frame_2.mask.png') == True
-    assert os.path.exists('tests\\images\\01_frame_2.blobs.png') == True
-    assert os.path.exists('tests\\images\\01_frame_2.blobs2.png') == True
+    md.applyFirstFrame('tests/images/01_frame_1.png')
+    md.checkMotion('tests/images/01_frame_2.png')
+    assert os.path.exists('tests/images/01_frame_2.clear.png') == True
+    assert os.path.exists('tests/images/01_frame_2.mask.png') == True
+    assert os.path.exists('tests/images/01_frame_2.blobs.png') == True
+    assert os.path.exists('tests/images/01_frame_2.blobs2.png') == True
     _removeTempFiles()
 
 
 def test_mask():
     md = MotionDetector(debug=True)
-    md.applyFirstFrame('tests\\images\\01_frame_1.png')
-    md.checkMotion('tests\\images\\01_frame_2.png')
+    md.applyFirstFrame('tests/images/01_frame_1.png')
+    md.checkMotion('tests/images/01_frame_2.png')
 
-    frameMask = cv2.imread('tests\\images\\01_frame_2.mask.png')
+    frameMask = cv2.imread('tests/images/01_frame_2.mask.png')
 
     (b, g, r) = frameMask[17, 65]
     assert int(r) + int(g) + int(b) == 765
@@ -94,10 +94,10 @@ def test_mask():
 
 def test_clear():
     md = MotionDetector(debug=True)
-    md.applyFirstFrame('tests\\images\\01_frame_1.png')
-    md.checkMotion('tests\\images\\01_frame_2.png')
+    md.applyFirstFrame('tests/images/01_frame_1.png')
+    md.checkMotion('tests/images/01_frame_2.png')
 
-    frameClear = cv2.imread('tests\\images\\01_frame_2.clear.png')
+    frameClear = cv2.imread('tests/images/01_frame_2.clear.png')
 
     (b, g, r) = frameClear[17, 65]
     assert int(r) + int(g) + int(b) == 0
